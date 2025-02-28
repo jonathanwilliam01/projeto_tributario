d83c9d0f5100c93a8c3fe9c882e0f203c1a2314b
Jonathan William <jonathan.willian@embras.net>
2025-02-28 15:49:06 -0300

    v1.102.04

---
+++ src/app/saiba-mais/saiba-mais.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saiba-mais',
+  standalone: true,
+  imports: [],
+  templateUrl: './saiba-mais.component.html',
+  styleUrl: './saiba-mais.component.scss'
+})
+export class SaibaMaisComponent {
+
+}
